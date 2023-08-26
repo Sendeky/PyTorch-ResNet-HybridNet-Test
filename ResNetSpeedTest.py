2c95c806c73e84b3161781f107ececae9ee5cdb9
Sendeky <spruslasha@gmail.com>
2023-08-26 16:00:21 -0700

    initial commit

---
+++ ResNetSpeedTest.py
@@ -0,0 +1,11 @@
+import torch
+from torchvision import transforms
+from PIL import Image
+
+model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+# or any of these variants
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet34', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet101', pretrained=True)
+# model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=True)
+model.eval()
